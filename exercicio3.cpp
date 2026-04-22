3112012afb7611d82af25e4ec2ee8b77272560e7
Fredericojunio <fredericoalbinojunior@gmail.com>
2026-04-22 17:02:34 +0200

    Refactor and add product search functionality

--- exercicio3.cpp
+++ exercicio3.cpp
@@ -1,46 +1,49 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
 int main() {
-    string nomes[5];
-    float precos[5];
-    int quantidades[5];
-    float subtotais[5];
-    
-    float total = 0;
+    int codigo[10];
+    int stock[10];
 
-    for(int i = 0; i < 5; i++) {
-        cout << "\nItem " << i+1 << endl;
-        cout << "Nome do item: ";
-        cin >> nomes[i];
-        
-        cout << "Preco unitario: ";
-        cin >> precos[i];
-        
-        cout << "Quantidade: ";
-        cin >> quantidades[i];
-        
-        subtotais[i] = precos[i] * quantidades[i];
-        total += subtotais[i];
+    int i, codigoPesquisa, encontrado;
+
+    // Leitura dos dados dos produtos
+    for (i = 0; i < 10; i++) {
+        printf("Digite o codigo do produto %d: ", i + 1);
+        scanf("%d", &codigo[i]);
+
+        printf("Digite o stock do produto %d: ", i + 1);
+        scanf("%d", &stock[i]);
     }
 
-    float desconto = 0;
-    if(total > 1000) {
-        desconto = total * 0.10;
-        total -= desconto;
-    }
+    // Pesquisa
+    printf("\n--- PESQUISA DE PRODUTO ---\n");
+    printf("Digite o codigo a procurar (ou -1 para sair): ");
+    scanf("%d", &codigoPesquisa);
 
-    cout << "\n--- Fatura ---\n";
-    for(int i = 0; i < 5; i++) {
-        cout << nomes[i] << ": " 
-             << quantidades[i] << " x " 
-             << precos[i] << " = " 
-             << subtotais[i] << " MT\n";
-    }
+    while (codigoPesquisa != -1) {
+        encontrado = 0;
 
-    cout << "\nTotal sem desconto: " << total + desconto << " MT";
-    cout << "\nDesconto: " << desconto << " MT";
-    cout << "\nTotal a pagar: " << total << " MT\n";
+        for (i = 0; i < 10; i++) {
+            if (codigo[i] == codigoPesquisa) {
+                printf("\nProduto encontrado!\n");
+                printf("Quantidade em stock: %d\n", stock[i]);
+
+                if (stock[i] < 5) {
+                    printf("AVISO: STOCK CRITICO!\n");
+                }
+
+                encontrado = 1;
+                break;
+            }
+        }
+
+        if (!encontrado) {
+            printf("Produto nao encontrado.\n");
+        }
+
+        printf("\nDigite outro codigo (ou -1 para sair): ");
+        scanf("%d", &codigoPesquisa);
+    }
 
     return 0;
 }
